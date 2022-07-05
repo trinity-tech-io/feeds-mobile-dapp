073508685db669ca7c4042af3891d2069c2b15bb
liaihong <unknown>
2022-07-06 00:24:59 +0800

    improver login twitter

--- src/app/services/TwitterService.ts
+++ src/app/services/TwitterService.ts
@@ -18,65 +18,43 @@ export class TwitterService {
   ) { }
 
   public getJsonFromUrl(url) {
-    if (!url) url = location.search;
-    var query = url.substr(1);
-    var result = {};
+    if (!url) url = location.search
+    var query = url.substr(1)
+    var result = {}
     query.split("&").forEach(function (part) {
-      var item = part.split("=");
-      result[item[0]] = decodeURIComponent(item[1]);
-    });
-    return result;
+      var item = part.split("=")
+      result[item[0]] = decodeURIComponent(item[1])
+    })
+    return result
+  }
+
+  public fetchTokenFromTwitterUseCode(code) {
+    let params = {
+      code: code,
+      grant_type: TwitterApi.GRANT_TYPE,
+      client_id: TwitterApi.CLIENT_ID,
+      redirect_uri: TwitterApi.REDIRECT_URI,
+      code_verifier: TwitterApi.CODE_VERIFIER
+    }
+    let header = {}
+    this.http.post(TwitterApi.TOKEN, params, header)
+      .then(data => {
+        const ddata = JSON.parse(data.data)
+        Logger.log(TAG, 'auth twitter success: ', data.data)
+        const accessToken1 = ddata.access_token
+        console.log("accessToken1 ===== ", accessToken1)
+        // TODO: 存储TOKEN DATA
+      })
+      .catch(error => {
+        Logger.error(TAG, "auth twitter Error: ", error)
+      })
   }
 
   public openTwitterLoginPage() {
-    const target = '_blank';
-    const options = 'location=no';
+    const target = '_system'
+    const options = 'location=no'
     console.log("inappBrowser 开始 >>>>>>>>>>>>>>>>>>>>>>>>>>>> ")
-
-    const browser = this.inappBrowser.create(TwitterApi.AUTH, target, options);
-    browser.on('loadstart').subscribe(event => {
-      console.log("inappBrowser 回调 结果 >>>>>>>>>>>>>>>>>>>>>>>>>>>> ")
-      console.log(event.url)
-      if (event.url.includes("www.baidu.com")
-        && event.url.includes("code=")) {
-        browser.hide();
-        const codeValue = this.getJsonFromUrl(event.url)["code"];
-        let params = {
-          code: codeValue,
-          grant_type: TwitterApi.GRANT_TYPE,
-          client_id: TwitterApi.CLIENT_ID,
-          redirect_uri: TwitterApi.REDIRECT_URI,
-          code_verifier: TwitterApi.CODE_VERIFIER
-        };
-        let header = {}
-        this.http.post(TwitterApi.TOKEN, params, header)
-          .then(data => {
-            console.log(data.status);
-            console.log(data.data); // data received by server
-            console.log(data.headers);
-            const ddata = JSON.parse(data.data);
-            console.log("ddata ===== ", ddata)
-            Logger.log(TAG, 'auth twitter success: ', data.data)
-            const accessToken1 = ddata.access_token
-            const accessToken2 = ddata['access_token']
-            const accessToken3 = ddata["access_token"]
-            console.log("accessToken1 ===== ", accessToken1)
-            console.log("accessToken2 ===== ", accessToken2)
-            console.log("accessToken3 ===== ", accessToken3)
-          })
-          .catch(error => {
-            console.log(error.status);
-            console.log(error.error); // error message as string
-            console.log(error.headers);
-            Logger.error(TAG, "auth twitter Error: ", error)
-
-          });
-      }
-
-      console.log("inappBrowser回调 结束 >>>>>>>>>>>>>>>>>>>>>>>>>>>> ")
-    }, err => {
-      alert("InAppBrowser loadstart Event Error: " + err);
-    });
+    this.inappBrowser.create(TwitterApi.AUTH, target, options)
   }
 
   public postTweet(text: string) {
