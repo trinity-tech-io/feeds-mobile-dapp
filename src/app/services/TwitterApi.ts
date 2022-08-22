558c09d168b62300be686b7453f8cdddae3e16fa
liaihong <>
2022-08-22 21:01:56 +0800

    replace twitter clientId

--- src/app/services/TwitterApi.ts
+++ src/app/services/TwitterApi.ts
@@ -1,12 +1,12 @@
 export class TwitterApi {
   public static GRANT_TYPE: string = "authorization_code"
   public static GRANT_TYPE_REFRESH: string = "refresh_token"
-  public static CLIENT_ID: string = "RDdwOVotcHZhQjFpTzlyWlJNTm46MTpjaQ"
+  public static CLIENT_ID: string = "Q2tUNTJzakF0S1RFY0otRVc4WEE6MTpjaQ"
   public static REDIRECT_URI: string = "https://feeds.trinity-feeds.app/login-from-twitter"
   public static CODE_VERIFIER: string = "challenge"
 
   // public static AUTH: string = "https://twitter.com/i/oauth2/authorize?response_type=code&client_id=MloxZFZUd21FWEJ1VHBnMkd3RHA6MTpjaQ&redirect_uri=https://feeds.trinity-feeds.app/logininsuccess&scope=tweet.read%20tweet.write%20users.read%20follows.read+follows.write%20offline.access&state=state&code_challenge=challenge&code_challenge_method=plain"
-  public static AUTH: string = "https://twitter.com/i/oauth2/authorize?response_type=code&client_id=RDdwOVotcHZhQjFpTzlyWlJNTm46MTpjaQ&redirect_uri=https://feeds.trinity-feeds.app/login-from-twitter&scope=tweet.read%20tweet.write%20users.read%20offline.access&state=state&code_challenge=challenge&code_challenge_method=plain"
+  public static AUTH: string = "https://twitter.com/i/oauth2/authorize?response_type=code&client_id=Q2tUNTJzakF0S1RFY0otRVc4WEE6MTpjaQ&redirect_uri=https://feeds.trinity-feeds.app/login-from-twitter&scope=tweet.read%20tweet.write%20users.read%20offline.access&state=state&code_challenge=challenge&code_challenge_method=plain"
 
   public static TOKEN: string = "https://api.twitter.com/2/oauth2/token"
 
